9036c4b08a23efbb583d78e9c2c2ef8e4a79d204
Alan  Maksymilian <114343803+AlanTheAlltaker@users.noreply.github.com>
2023-11-02 14:55:05 +0100

    Add files via upload

---
+++ Script/ChangeTeamCube.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Globalization;
+using Unity.Netcode;
+using UnityEngine;
+
+public class ChangeTeamCube : NetworkBehaviour, IInteractable
+{
+    [SerializeField] Material red;
+    [SerializeField] Material blue;
+    public void Interact()
+    {
+        ChangeColorServerRpc();
+    }
+    [ServerRpc(RequireOwnership = false)]
+    public void ChangeColorServerRpc(ServerRpcParams serverRpcParams = default)
+    {
+        var clientId = serverRpcParams.Receive.SenderClientId;
+        if (NetworkManager.ConnectedClients.ContainsKey(clientId))
+        {
+            var client = NetworkManager.ConnectedClients[clientId];
+            NetworkObject player = NetworkManager.Singleton.ConnectedClients[NetworkManager.Singleton.LocalClientId].PlayerObject;
+            Renderer playerRenderer = player.GetComponent<Renderer>();
+            TagsManager tagsManager = player.GetComponent<TagsManager>();
+            if (tagsManager.tagsList.Contains("Blue") && !tagsManager.tagsList.Contains("Red"))
+            {
+                tagsManager.tagsList.Add("Red");
+                tagsManager.tagsList.Remove("Blue");
+                Debug.Log("Dodano Red dla gracza");
+
+                playerRenderer.material = red;
+            } else if (tagsManager.tagsList.Contains("Red") && !tagsManager.tagsList.Contains("Blue"))
+            {
+                tagsManager.tagsList.Add("Blue");
+                tagsManager.tagsList.Remove("Red");
+                Debug.Log("Dodano Blue dla gracza");
+
+                playerRenderer.material = blue;
+            }
+        }
+    }
+}
