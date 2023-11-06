23a048dbd6ac26bf8fe5da1a51868229bdf51963
Alan  Maksymilian <114343803+AlanTheAlltaker@users.noreply.github.com>
2023-11-06 14:39:20 +0100

    Update health i bullet

---
+++ Script/GunCannon.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Netcode;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class GunCannon : NetworkBehaviour
+{
+    public Transform bulletSpawnPoint;
+    public GameObject bulletPrefab;
+    public float bulletSpeed = 10;
+
+    [SerializeField] float cooldownTime = 0f;
+    bool isCooldown = false;
+
+    void Update()
+    {
+        if (!IsOwner) { return; }
+        if (Input.GetMouseButtonDown(0) && !isCooldown)
+        {
+            StartCoroutine(Cooldown());
+            ShootServerRpc(bulletSpawnPoint.position, bulletSpawnPoint.rotation);
+        }
+    }
+    IEnumerator Cooldown()
+    {
+        isCooldown = true;
+        yield return new WaitForSeconds(cooldownTime);
+        isCooldown = false;
+    }
+    [ServerRpc (RequireOwnership = false)]
+    void ShootServerRpc(Vector3 position, Quaternion rotation)
+    {
+        GameObject bullet = Instantiate(bulletPrefab, position, rotation);
+        bullet.GetComponent<NetworkObject>().Spawn();
+        //bullet.GetComponent<Rigidbody>().isKinematic = false;
+        //bullet.GetComponent<Rigidbody>().velocity = bulletSpawnPoint.forward * bulletSpeed;
+    }
+}
